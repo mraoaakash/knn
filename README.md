f1d5df8aa0edfcfcfb8c34608f05aaa4f44a8ee5
Aakash Rao <mrao.aakash@gmail.com>
2022-11-08 21:41:58 +0530

    Added the starter point

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
-# knn
+# k-Nearest Neighbours
+This is a repository that contains the code for the k-nearest neighbours algorithm coded from scratch for CS-1390 [Intruduction to Machine Learning]. Here we will use a value of k=5. To run the code, first place the dataset available at https://www.kaggle.com/code/waltermaffy/fruit-classification-pca-svm-knn-decision-tree/notebook in the following folder ```./knn/input```. 
+
+
--- knn.py
+++ knn.py
@@ -1,23 +1,8 @@
-#Dependencies
-import math
-import os
-import sys
-import numpy as np 
-import cv2
-import glob
-import os
-import matplotlib.pyplot as plt
-import string
-from mlxtend.plotting import plot_decision_regions
+import numpy as np
+from builtins import object
+from builtins import range
 
 
-
-def euclidean_distance(x1, x2, y1, y2):
-    return math.sqrt((x2 - x1) ** 2 + (y2 - y1) ** 2)
-
-def datacheck():
-    print(os.listdir("./input"))
-    return 
-
-# print(euclidean_distance(1, 2, 3, 4))
-datacheck()
+class knn(object):
+    def __init__(self):
+        pass
