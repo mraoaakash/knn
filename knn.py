5320b5317e6414d096ad64be1c00b16f90e4be07
Aakash Rao <mrao.aakash@gmail.com>
2022-11-09 00:15:21 +0530

    code completed

--- knn.py
+++ knn.py
@@ -1,9 +1,9 @@
 import numpy as np
-from builtins import object
+from builtins import object as py_object
 from builtins import range
 import cv2
 import os 
-class knn(object):
+class knn(py_object):
     def __init__(self):
         pass
     
@@ -16,7 +16,8 @@ class knn(object):
         n_test = X.shape[0]
         distance = np.zeros((n_test, n_train))
         for i in range(n_train):
-            distance[i, :] = np.sqrt(np.sum(np.square(self.X_train - X[i, :]), axis=1))
+            for j in range(n_test):
+                distance[j, i] = np.sqrt(np.sum((X[j, :] - self.X_train[i, :])**2))
             pass
         return distance 
 
@@ -24,36 +25,45 @@ class knn(object):
         n_test = distance.shape[0]
         predictions = np.zeros(n_test)
         for i in range(n_test):
-            predictions[i] = np.argmax(np.bincount(self.y_train[np.argsort(distance[i, :])[:k]]))
+            closest = np.argsort(distance[i, :])[:k]
+            predictions[i] = np.argmax(np.bincount(self.y_train[closest]))
         return predictions
     
 
-train_path = "input/Training"
-test_path = "input/Test"
+train_path = ["./input/Training/Apple Red Delicious", "./input/Training/Avocado"]
+test_path = ["./input/Test/Apple Red Delicious", "./input/Test/Avocado"]
 
-X_train = []
-y_train = []
 
-for i in os.walk(train_path):
-    for j in i[2]:
-        img = cv2.imread(f"{i[0]}/{j}")
-        X_train.append(img)
-        y_train.append(i[0].split("/")[-1])
-        pass
+def load_data(path):
+    images = []
+    labels = []
+    for i in path:
+        for j in os.listdir(i):
+            img = cv2.imread(i + "/" + j)
+            images.append(img)
+            labels.append(path.index(i))
+    return images, labels
 
-train_path = "input/Training"
-test_path = "input/Test"
+x_train, y_train = load_data(train_path)
+x_train = np.array(x_train)
+x_train = x_train.reshape(x_train.shape[0], -1)
+y_train = np.array(y_train)
+y_train = y_train.reshape(y_train.shape[0])
 
-X_test = []
-y_test = []
+x_test, y_test = load_data(test_path)
+x_test = np.array(x_test)
+x_test = x_test.reshape(x_test.shape[0], -1)
+y_test = np.array(y_test)
+y_test = y_test.reshape(y_test.shape[0])
 
-for i in os.walk(test_path):
-    print(i[0])
-    for j in i[2]:
-        img = cv2.imread(f"{i[0]}/{j}")
-        X_test.append(img)
-        y_test.append(i[0].split("/")[-1])
-        pass
+print(f"Training data loaded {np.array(x_train).shape}")
+print(f"Training Labels loaded {np.array(y_train).shape}")
+print(f"Testing data loaded {np.array(x_test).shape}")
+print(f"Testing labels loaded {np.array(y_test).shape}")
 
-# print(X_test)
-# print(y_test)
+model = knn()
+model.train(x_train, y_train)
+distance = model.distances(x_test)
+predictions = model.predict(distance, k=1)
+accuracy = np.mean(predictions == y_test)
+print(f"Accuracy: {accuracy}")
