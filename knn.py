df0d6df1b5819b55a5637d25da1031933b58bc6f
Aakash Rao <mrao.aakash@gmail.com>
2022-11-08 22:12:17 +0530

    Added train data loader

--- knn.py
+++ knn.py
@@ -1,8 +1,8 @@
 import numpy as np
 from builtins import object
 from builtins import range
-
-
+import cv2
+import os 
 class knn(object):
     def __init__(self):
         pass
@@ -20,10 +20,26 @@ class knn(object):
             pass
         return distance 
 
-    def predict_labels(self, distance, k=1):
+    def predict(self, distance, k=1):
         n_test = distance.shape[0]
         predictions = np.zeros(n_test)
         for i in range(n_test):
             predictions[i] = np.argmax(np.bincount(self.y_train[np.argsort(distance[i, :])[:k]]))
         return predictions
     
+
+train_path = "input/Training"
+test_path = "input/Test"
+
+X_train = []
+y_train = []
+
+for i in os.walk(train_path):
+    for j in i[2]:
+        img = cv2.imread(f"{i[0]}/{j}")
+        X_train.append(img)
+        y_train.append(i[0].split("/")[-1])
+        pass
+
+print(X_train)
+print(y_train)
